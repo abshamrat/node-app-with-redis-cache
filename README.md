37f93f722e510465261a981c5f4376f89ce9a336
shamrat17 <shamratakbar17@gmail.com>
2018-12-11 16:22:02 +0600

    redis connection established

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # node-app-with-redis-cache
 Super fast web application in nodejs using redis cache
 
+<!-- docker run --name redis -p 6379:6379 -d redis  -->
+
 Helper link [here](https://blog.manifold.co/build-super-fast-apps-in-node-js-using-redis-cache-d3d55bbdc375)
--- server.js
+++ server.js
@@ -4,7 +4,23 @@ const axios = require('axios');
 const redis = require('redis');
 const app = express();
 
+
+const port = process.env.port || 5000;
+const client = redis.createClient(REDIS_URL);
+const REDIS_URL = process.env.REDIS_URL || "http://localhost:6379";
 const API_URL = 'https://api.exchangeratesapi.io';
+
+client.on('connect', () => {
+    console.log(`connected to redis`);
+});
+client.on('error', err => {
+    console.log(`Error: ${err}`);
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+});
+
 app.get('/', (req, res) => {
     res.sendFile('index.html', {
         root: path.join(__dirname, 'views')
@@ -13,16 +29,32 @@ app.get('/', (req, res) => {
 app.get('/rate/:date', (req, res) => {
     const date = req.params.date;
     const url = `${API_URL}/${date}?base=USD`;
-    axios.get(url).then(response => {
-        return res.json({ rates: response.data.rates });
-    }).catch(error => {
-        console.log(error);
+    const countKey = `USD:${date}:count`;
+  const ratesKey = `USD:${date}:rates`;
+  
+  client.incr(countKey, (err, count) => {
+    client.hgetall(ratesKey, function(err, rates) {
+      if (rates) {
+        return res.json({ rates, count });
+      }
+      axios.get(url).then(response => {
+        // save the rates to the redis store
+        client.hmset(
+          ratesKey, response.data.rates, function(err, result) {
+          if (err) console.log(err);
+        });
+
+        return res.json({ 
+          count,
+          rates: response.data.rates
+        });
+      })
+      .catch(error => {
+        return res.json(error.response.data)
+      });
+
     });
+  });
 
 });
 
-const port = process.env.port || 5000;
-
-app.listen(port, () => {
-    console.log(`App listening on port ${port}!`)
-});
