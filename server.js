2171bb274e9520dc5dfb461de6b68c3d3f8e0169
shamrat17 <shamratakbar17@gmail.com>
2018-12-12 14:28:43 +0600

    bluebird added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const bluebird = require("bluebird");
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
@@ -6,9 +7,13 @@ const app = express();
 
 
 const port = process.env.port || 5000;
-const client = redis.createClient(REDIS_URL);
 const REDIS_URL = process.env.REDIS_URL || "http://localhost:6379";
 const API_URL = 'https://api.exchangeratesapi.io';
+
+const client = redis.createClient(REDIS_URL);
+// make node_redis promise compatible
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
 
 client.on('connect', () => {
     console.log(`connected to redis`);
@@ -32,29 +37,35 @@ app.get('/rate/:date', (req, res) => {
     const countKey = `USD:${date}:count`;
   const ratesKey = `USD:${date}:rates`;
   
-  client.incr(countKey, (err, count) => {
-    client.hgetall(ratesKey, function(err, rates) {
+  let count;
+  client
+    .incrAsync(countKey)
+    .then(result => {
+      count = result;
+      return count;
+    })
+    .then(() => client.hgetallAsync(ratesKey))
+    .then(rates => {
       if (rates) {
         return res.json({ rates, count });
       }
-      axios.get(url).then(response => {
-        // save the rates to the redis store
-        client.hmset(
-          ratesKey, response.data.rates, function(err, result) {
-          if (err) console.log(err);
-        });
 
-        return res.json({ 
+      axios.get(url).then(response => {
+        client
+          .hmsetAsync(ratesKey, response.data.rates)
+          .catch(e => {
+            console.log(e)
+          });
+        return res.json({
           count,
           rates: response.data.rates
         });
-      })
-      .catch(error => {
-        return res.json(error.response.data)
-      });
+      }).catch(error => res.json(error.response.data))
 
+    })
+    .catch(e => {
+      console.log(e)
     });
-  });
 
 });
 
